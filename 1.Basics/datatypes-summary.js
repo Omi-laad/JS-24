05bd5de62ec4ce65f015797aaf4a2332cde5c631
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 07:45:28 +0000

    datatypes-summary

---
+++ 1.Basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+//Summary for datatypes
+
+//primitive 
+// 7 types : String ,Number , Boolean , null , undefined , Symbol , BigInt
+
+
+
+const score = 100
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const anotherid = Symbol('123')
+
+
+console.log(id === anotherid);
+
+
+//Reference (Non primitive):
+
+// Arrays,Objects ,Functions
+
+const heros =["Batman","Superman","Flash"]
+
+
+
+
+let myObj ={
+    name:"omkar",
+    age:20,
+}
+
+
+
+const myfunction= function(){
+
+
+}
+
+console.log(typeof myfunction);
