0dd5ec84fee5fab0f1b332fa10bcd00c71210907
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 07:59:16 +0000

    memory(stack,heap)

--- 1.Basics/datatypes-summary.js
+++ 1.Basics/datatypes-summary.js
@@ -15,7 +15,7 @@ const id =Symbol('123')
 const anotherid = Symbol('123')
 
 
-console.log(id === anotherid);
+//console.log(id === anotherid);
 
 
 //Reference (Non primitive):
@@ -39,4 +39,35 @@ const myfunction= function(){
 
 }
 
-console.log(typeof myfunction);
+//console.log(typeof myfunction);
+
+
+//*************************MEMORIES***********************//
+
+
+//STACK (Primitive) , HEAP(Non-Primitive)
+
+let myname = "Omkar";
+
+let othername = myname
+othername = "Laad"
+
+
+console.log(myname);
+console.log(othername);
+
+
+
+let user ={
+
+    email:"abc@gmai.com", 
+    upi:"userupi1"
+}
+
+
+let user2= user
+
+user2.email = "aosmsgmail.com"
+
+console.log(user.email)
+console.log(user2.email)
