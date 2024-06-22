58fe56faf87725ce71e7ef65db223b1c544c9715
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 08:12:33 +0000

    strings

---
+++ 1.Basics/strings.js
@@ -0,0 +1,14 @@
+const name = "omkar"
+
+const repocount = 10
+
+// console.log(name + repocount + "Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount} `)
+
+
+const myName = new String('OMI-Laad')
+console.log(myName);
+
+console.log(myName.__proto__);
+console.log(myName.toLowerCase());
