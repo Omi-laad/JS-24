c91c90e2d14f95f1b3e52e5156a1dad7bd182a98
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 06:19:04 +0000

    Conversiondatatypes

---
+++ 1.Basics/conversionsOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+/*  
+"22"=> 22
+"22abc" => Nan(NOT A NUMBER)
+true => 1; false => 0;
+
+*/
+
+
+
+
+isLoggedIn = "omkar"
+let boolenisLoggedIn =Boolean(isLoggedIn)
+//console.log(boolenisLoggedIn)
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
