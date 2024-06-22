e8884cb5f8ea9586eb04bbed82a0d9e28401e2b3
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 10:19:36 +0000

    string-methodfunctions

--- 1.Basics/strings.js
+++ 1.Basics/strings.js
@@ -12,3 +12,20 @@ console.log(myName);
 
 console.log(myName.__proto__);
 console.log(myName.toLowerCase());
+
+
+const nme = myName.substring(1,2)
+console.log(nme)
+
+
+const newstring1 = " omkar " 
+console.log(newstring1)
+
+console.log(newstring1.trim())
+
+
+
+const url = "https://omkar.com/jasdod%20ele"
+console.log(url.replace('%20',''));
+console.log(url.includes('laad'))
+
