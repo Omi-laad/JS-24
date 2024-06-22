92f76cfeb2d89e486857bb04df70b8caaf564d34
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 06:02:59 +0000

    datatypes

---
+++ 1.Basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(9+0)  //cant use this cause we r using node and it cant be applied in it only for browser
+
+
+console.log(9-8)
+
+console.log("Omkar");
+
+let name = "omkar"
+let age = 20
+let isLoggedIn = false
+ 
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value   considerd as a object
+//undefined => 
+//symbol => unique
+
+
+//object
+console.log(typeof number);
+// if number is not declared then it is undefined for that moment
+let number = 12
+console.log(typeof age);
+console.log(typeof null); //  as object
+console.log(typeof number);
