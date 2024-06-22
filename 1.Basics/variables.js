58d5153169e091a7dc7dcb01eacf33ed9a88f933
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-22 05:48:45 +0000

    Variables in Js

---
+++ 1.Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 4548451 
+let accountEmail = "omkar@gmail.com"
+var accountPassword = "1233"
+accountCity = "Mumbai"
+let accountState;
+//javascript allows a variable to get memory reserve without any const,let   but a bad habit
+
+//  accountId = 2  cant do it cause already declared above with const
+/*
+dont use Var issue with scope
+
+*/
+
+accountEmail = "laad@gmail.com"
+accountPassword = "21213"
+accountCity = "Indore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
