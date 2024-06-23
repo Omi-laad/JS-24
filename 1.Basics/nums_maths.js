ae6106451d90c676e32ea1ece98d0e49eaa6f069
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-06-23 13:39:19 +0000

    num_maths

---
+++ 1.Basics/nums_maths.js
@@ -0,0 +1,14 @@
+const score = 200
+
+console.log(score);
+
+const balance = new Number(100)
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const otherNumber =25.394
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
