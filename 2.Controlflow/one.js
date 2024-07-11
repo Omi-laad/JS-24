fb567dcd7abb2b32f02d5257575f0ca38ed087bb
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-07-11 14:29:38 +0000

    if-else

---
+++ 2.Controlflow/one.js
@@ -0,0 +1,66 @@
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature===41)
+//     {
+//     console.log("less then 50");
+//     }
+// else{
+// console.log("more then 50");
+// }
+
+
+// <,> <=,=>,!=,==
+//!= negative checking
+// == is for checking
+//=== is for strict checking
+//= operator assignment
+
+
+
+// const score = 300
+// if (score>100){
+//     const power = "fly"
+//     console.log(`User power :${power}`);
+// }
+// console.log(`User power :${power}`);
+
+
+// const balance = 1000
+// // if(balance>500){
+// //     console.log("test");
+// // }
+
+// if ( balance <500){
+//     console.log("less then");
+
+// }
+// else if (balance >750){
+//     console.log("more then 750");
+// }
+// else{
+//     console.log("more then 500");
+// }
+
+
+
+
+
+
+
+const userLoggedIn= true
+const debitCard= true
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+// if (userLoggedIn && debitCard) {
+    // console.log("allowed");
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+   
+   
+    console.log("allowed");
+    
+}
