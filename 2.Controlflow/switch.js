41aa461ee27ed8f789a67a8327fc0321251f4e96
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-07-11 14:37:38 +0000

    switch condition

---
+++ 2.Controlflow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "april"
+
+switch (month) {
+    case 1:
+         console.log("jan");
+        break;
+    case 2:
+         console.log("feb");
+        break;
+    case 3:
+         console.log("march");
+        break;
+    case "april":
+         console.log("april");
+        break;
+
+    default:
+        console.log("default case macthed");
+        break;
+}
