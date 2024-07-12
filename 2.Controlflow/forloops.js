0598bd565b5b5020ff6d78b1e7dee4be7c047280
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-07-12 13:27:54 +0000

    for loop

---
+++ 2.Controlflow/forloops.js
@@ -0,0 +1,26 @@
+// for loop
+
+for (let i = 0; i <=  10; i++) 
+{
+    const element = i;    
+    if (element==5) {
+       // console.log("5 is best");
+    }
+    //console.log(element);
+        
+        
+}
+
+for (let i=0 ;i<= 10; i++) {
+
+//console.log(`Outer loop ${i}`);
+for (let j = 0; i<=10; j++) {
+   // console.log(`inner loop ${i}`);
+}
+}
+
+for (let i = 0; i <=20; i++) {
+
+console.log(i);
+
+}
