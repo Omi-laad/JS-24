30baca90e3fc31bc34cffbcc5051460c46d924c7
omkarlaad <109172830+Omi-laad@users.noreply.github.com>
2024-07-13 19:50:39 +0000

    filte rmap pt1

---
+++ 2.Controlflow/six.js
@@ -0,0 +1,15 @@
+// const myArr =["js","rb","c","c++"]
+
+// const values = myArr.forEach( (item)=>{
+//    console.log(item);
+//     return
+// }  )
+
+// console.log(values);
+
+
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.filter(  (num) => num > 5 )
+
+console.log(newNums);
